b0d0cf693f5f01637f9c871adc6ba8a4051a7321
TCSL06 <w2da130@uos.ac.kr>
2018-12-16 16:16:34 +0900

    Matrix4.mulVector4를 구현했는데 뭔가 안맞는다.. 인덱스문제? 리턴문제? ...

--- ch07/LookAtTrianglesWithKeys.js
+++ ch07/LookAtTrianglesWithKeys.js
@@ -115,8 +115,12 @@ function initVertexBuffers(gl) {
 }
 
 var g_eyeX = 0.20, g_eyeY = 0.25, g_eyeZ = 0.25; // Eye position
-function keydown(ev, gl, n, u_ViewMatrix, viewMatrix) {
+var vecEye = new Float32Array(4);
+vecEye[0] = g_eyeX; vecEye[1] = g_eyeY; vecEye[2] = g_eyeZ;
+var rotMatrix = new Matrix4();
 
+var cenX = 0.0 , cenY = 0.0 , cenZ = 0.0 , upX = 0.0 , upY = 1.0 , upZ = 0.0;
+function keydown(ev, gl, n, u_ViewMatrix, viewMatrix) {
     if(ev.keyCode == 38) { // The right up key was pressed
       console.log('u');
 
@@ -125,19 +129,25 @@ function keydown(ev, gl, n, u_ViewMatrix, viewMatrix) {
     if(ev.keyCode == 40) { // The right down key was pressed
       console.log('d');
     } else 
-
+    
+    var angle = 10.0;
+    var tempVec = new Float32Array(4)
     if(ev.keyCode == 39) { // The right arrow key was pressed
-      g_eyeX += 0.01;
+      rotMatrix.rotate(angle, upX, upY, upZ);
+      rotMatrix.mulVector4(vecEye);
+      console.log('check return value of rotMatrix', rotMatrix);
+
     } else 
     if (ev.keyCode == 37) { // The left arrow key was pressed
-      g_eyeX -= 0.01;
+      tempMatrix.rotate(-angle, upX, upY, upZ);
+      //g_eyeX -= 0.01;
     } else { return; }
     draw(gl, n, u_ViewMatrix, viewMatrix);    
 }
 
 function draw(gl, n, u_ViewMatrix, viewMatrix) {
   // Set the matrix to be used for to set the camera view
-  viewMatrix.setLookAt(g_eyeX, g_eyeY, g_eyeZ, 0, 0, 0, 0, 1, 0);
+  viewMatrix.setLookAt(g_eyeX, g_eyeY, g_eyeZ, cenX, cenY, cenZ, upX, upY, upZ);
 
   // Pass the view projection matrix
   gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
@@ -147,25 +157,6 @@ function draw(gl, n, u_ViewMatrix, viewMatrix) {
   gl.drawArrays(gl.TRIANGLES, 0, n); // Draw the rectangle
 }
 
-
-testVector4 = new Vector4([1, 2, 3,4]);
-console.log(testVector4);
-
-
-Vector3.prototype.testFunc = function() {
-  var v = this.elements;
-  var c = v[0], d = v[1], e = v[2], g = Math.sqrt(c*c+d*d+e*e);
-  if(g){
-    if(g == 1)
-        return this;
-   } else {
-     v[0] = 0; v[1] = 0; v[2] = 0;
-     return this;
-   }
-   g = 1/g;
-   v[0] = c*g; v[1] = d*g; v[2] = e*g;
-   return this;
-};
 
 
 
@@ -188,7 +179,7 @@ Vector4.prototype.add = function(vecB) {
 
 
 Vector4.prototype.cross = function(vecB) {
-    var e, fx, fy, fz, sx, sy, sz, ux, uy, uz;
+    var fx, fy, fz, sx, sy, sz, ux, uy, uz;
     
     s = this.elements;
     sx = s[0];
@@ -208,6 +199,23 @@ Vector4.prototype.cross = function(vecB) {
     this.elements = u;
 }
 
+Matrix4.prototype.mulVector4 = function(vecB) {
+  var e = this.elements;
+  console.log('this.elements before update', this.elements);
+  var p = vecB.elements;
+
+  var u = new Float32Array(4);
+  u[0] = p[0] * e[0] + p[1] * e[4] + p[2] * e[ 8] + p[3] * e[12];
+  u[1] = p[0] * e[1] + p[1] * e[5] + p[2] * e[ 9] + p[3] * e[13];
+  u[2] = p[0] * e[2] + p[1] * e[6] + p[2] * e[10] + p[3] * e[14];
+  u[3] = p[0] * e[3] + p[1] * e[7] + p[2] * e[11] + p[3] * e[15];
+
+  
+  this.elements = u;
+  console.log('this.elements after update', this.elements);
+  return this;
+};
+/*
 testVec4 = new Vector4([10, 10, 10, 10]);
 
 testVec4_B = new Vector4([0,1,0, 1]);
@@ -216,3 +224,46 @@ console.log('add vec4 test', testVec4);
 /*
 testVec4.cross(testVec4_B);
 console.log('updated cross', testVec4);*/
+/*
+testMatrixA = new Matrix4();
+testMatrixA.setRotate(90, 0, 0, 1);
+
+//testMatrixA.mulVector4(testVec4);
+testMatrixA.mulVector4(new Vector4([1,0,0,1]));
+console.log('check return value' , testMatrixA);
+*/
+
+/*
+function keydown(ev, gl, n, u_ViewMatrix, viewMatrix) {
+
+    if(ev.keyCode == 38) { // The right up key was pressed
+      console.log('u');
+
+    } else 
+
+    if(ev.keyCode == 40) { // The right down key was pressed
+      console.log('d');
+    } else 
+    
+    var angle = 10.0;
+    var rotMatrix = new Matrix4();
+    var vecUp = new Vector4();
+    vecUp[0] = upX; vecUp[1] = upY; vecUp[2] = upZ;
+
+    var tempVec = new Float32Array(4)
+    if(ev.keyCode == 39) { // The right arrow key was pressed
+      
+  
+      rotMatrix.setRotate(angle, upX, upY, upZ);
+      tempVec.elements = rotMatrix.mulVector4(vecEye);
+      console.log('multiplyVector4 updated tempVec', tempVec);
+      //g_eyeX = tempMatrix.multiplyVector4(Vector4([1,0,0,1]));
+      //vecEye.elements = tempMatrix;
+    } else 
+    if (ev.keyCode == 37) { // The left arrow key was pressed
+      tempMatrix.rotate(-angle, upX, upY, upZ);
+      //g_eyeX -= 0.01;
+    } else { return; }
+    draw(gl, n, u_ViewMatrix, viewMatrix);    
+}
+*/
